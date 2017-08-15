407587cc6924299676737d77065d1476f918707a
takeuchiken <takeuchiken@as-macbook-pro.local>
2017-08-15 21:16:19 +0900

    Initial Commit

---
+++ Swift-Local_WebView-Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Local_WebView-Sample
+//
+//  Created by 竹内健 on 2017/08/15.
+//  Copyright © 2017年 竹内健. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Swift-Local_WebView-SampleTests/Swift_Local_WebView_SampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_Local_WebView_SampleTests.swift
+//  Swift-Local_WebView-SampleTests
+//
+//  Created by 竹内健 on 2017/08/15.
+//  Copyright © 2017年 竹内健. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_Local_WebView_Sample
+
+class Swift_Local_WebView_SampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
